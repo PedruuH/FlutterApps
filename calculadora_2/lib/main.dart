c7e892ef796668537e2cc428b75e29d98c768e21
PedruuH <pedro.inventador@hotmail.com>
2021-10-11 13:56:00 -0300

    final

--- calculadora_2/lib/main.dart
+++ calculadora_2/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    home: Home()
-  ));
-      
+void main() {
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatefulWidget {
@@ -15,40 +12,116 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+
+  String _infoText = "Informe seus dados";
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _resetFields() {
+    pesoController.text = "";
+    alturaController.text = "";
+    setState(() {
+      _infoText = "Informe seus dados";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _calcular() {
+    setState(() {
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text) / 100;
+      double imc = peso / (altura * altura);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade II (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 39.9) {
+        _infoText = "Obesidade III (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Calculadora de IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.blueGrey,
-        actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.refresh))
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Icon(Icons.person, size: 120.0),
-          const TextField(keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-            labelText: "Peso em Kg",
-            labelStyle: TextStyle(color: Colors.black)
-          ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black, fontSize: 20.0),
-          ),
-          const TextField(keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Altura em cm",
-                labelStyle: TextStyle(color: Colors.black,fontSize: 19.0)
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black, fontSize: 20.0),
-          )
-        ]
-      )
-    );
+        appBar: AppBar(
+          title: const Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey,
+          actions: [
+            IconButton(onPressed: _resetFields, icon: const Icon(Icons.refresh))
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Icon(Icons.person, size: 120.0),
+                      TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                            labelText: "Peso em Kg",
+                            labelStyle: TextStyle(color: Colors.black)),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            color: Colors.black, fontSize: 20.0),
+                        controller: pesoController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Insira um dado";
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                            labelText: "Altura em cm",
+                            labelStyle:
+                                TextStyle(color: Colors.black, fontSize: 19.0)),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            color: Colors.black, fontSize: 20.0),
+                        controller: alturaController,
+                        validator: (value) {
+                          if (value==null || value.isEmpty) {
+                            return "Insira um dado";
+                          }
+                          return null;
+                        },
+                      ),
+                      Padding(
+                          padding:
+                              const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                          //descolar botão do TextField
+                          child: Container(
+                              height: 45.0,
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    if (_formKey.currentState!.validate()) {
+                                      _calcular();
+                                    }
+                                  },
+                                  child: const Text(
+                                    'Calcular',
+                                    style: TextStyle(fontSize: 15.0),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                      primary: Colors.green)))),
+                      Text(_infoText,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.green, fontSize: 25.0))
+                    ]))));
   }
 }
